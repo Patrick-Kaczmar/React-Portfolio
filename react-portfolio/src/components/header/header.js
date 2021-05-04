85e09800d7e50d82ac02c3c8585da466e52f4ac5
Zhylow <pk1blue@yahoo.com>
2021-05-04 18:34:48 -0400

    mad navbar sticky

--- react-portfolio/src/components/header/header.js
+++ react-portfolio/src/components/header/header.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav id="parentNav" className="navbar navbar-expand-lg navbar-dark bg-dark">
             <a className="navbar-brand" href="/">Home</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="https://github.com/navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
--- react-portfolio/src/components/project/project.js
+++ react-portfolio/src/components/project/project.js
@@ -58,18 +58,18 @@ function Project() {
             <div className="row">
                 {myProjects.map(project => (
                     <div key={project.title} className="col col-sm-12 col-md-6 col-xl-4">
-                        <div className="card" style={{ width: "22rem", height: "450px", marginBottom: "20px"}}>
+                        <div className="card">
                             <img src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg" className="card-img-top" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title">Project title</h5>
+                                <p className="card-text">Description for the project.</p>
                             </div>
                             <ul className="list-group list-group-flush">
-                                <li className="list-group-item">An item</li>
+                                <li className="list-group-item">technologies used</li>
                             </ul>
                             <div className="card-body">
-                                <a href="#" className="card-link">Card link</a>
-                                <a href="#" className="card-link">Another link</a>
+                                <a href="/" className="card-link">github repo link</a>
+                                <a href="/" className="card-link">active site link</a>
                             </div>
                         </div>
                     </div>
