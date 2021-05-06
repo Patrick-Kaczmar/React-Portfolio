fba60b35ff03701db15c470b8f776eceadc2d84a
Zhylow <pk1blue@yahoo.com>
2021-05-06 18:40:57 -0400

    added about me page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/header/header.js";
 import Project from "./components/project/project.js"
 import "./assets/main.css"
 import Footer from "./components/footer/footer.js";
+import AboutMe from "./components/aboutMe/aboutMe.js";
 
 function App() {
   return (
     <>
     <Header/>
-    <Project/>
+    {/* <Project/> */}
+    <AboutMe/>
     <Footer/>
     </>
   )
--- react-portfolio/src/components/footer/footer.js
+++ react-portfolio/src/components/footer/footer.js
@@ -4,9 +4,9 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <p class="inLine">My email: <a id="myEmail" href="/" onclick="return false">pk1blue@yahoo.com</a></p>
-            <p id="copyRight" class="inLine">My Phone# 845-545-4752</p>
-            <a class=" inLine" href="https://drive.google.com/file/d/1h9YONuBULUaO7lHalEniQgOY9K7FNCGo/view?usp=sharing" rel="noreferrer" target="_blank"><span class="navUniform">My Resume</span></a>
+            <p className="inLine">My email: <a id="myEmail" href="/">pk1blue@yahoo.com</a></p>
+            <p id="copyRight" className="inLine">My Phone# 845-545-4752</p>
+            <a className=" inLine" href="https://drive.google.com/file/d/1h9YONuBULUaO7lHalEniQgOY9K7FNCGo/view?usp=sharing" rel="noreferrer" target="_blank"><span className="navUniform">My Resume</span></a>
         </footer>
     )
 }
