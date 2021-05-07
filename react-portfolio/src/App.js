c22f92beb9fddd8274da63209f4baac6dfdc1fba
Zhylow <pk1blue@yahoo.com>
2021-05-07 19:45:39 -0400

    added hashing router

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,16 +4,18 @@ import Project from "./components/project/project.js"
 import "./assets/main.css"
 import Footer from "./components/footer/footer.js";
 import AboutMe from "./components/aboutMe/aboutMe.js";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter, HashRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Route path="/" component={Header} />
-      <Route exact path="/" component={Project} />
-      <Route exact path="/aboutme" component={AboutMe} />
-      <Route path="/" component={Footer} />
-    </Router>
+    <HashRouter basename="ReactPortfolio">
+      <Router>
+        <Route path="/" component={Header} />
+        <Route exact path="/" component={Project} />
+        <Route exact path="/aboutme" component={AboutMe} />
+        <Route path="/" component={Footer} />
+      </Router>
+    </HashRouter>
   )
 }
 
