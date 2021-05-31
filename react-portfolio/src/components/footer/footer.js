6ef3874447cd3b52d4f81ffe1734cbaa9d577eef
Zhylow <pk1blue@yahoo.com>
2021-05-30 20:44:14 -0400

    changed portfolio link

--- react-portfolio/src/components/footer/footer.js
+++ react-portfolio/src/components/footer/footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <footer>
             <p className="inLine">My email: <a id="myEmail" href="/">pk1blue@yahoo.com</a></p>
             <p id="copyRight" className="inLine">My Phone# 845-545-4752</p>
-            <a className=" inLine" href="https://drive.google.com/file/d/1h9YONuBULUaO7lHalEniQgOY9K7FNCGo/view?usp=sharing" rel="noreferrer" target="_blank"><span className="navUniform">My Resume</span></a>
+            <a className=" inLine" href="https://drive.google.com/file/d/1Q4s-7X0M4wZqPPkuKNLE6PmPDnX_935T/view?usp=sharing" rel="noreferrer" target="_blank"><span className="navUniform">My Resume</span></a>
         </footer>
     )
 }
