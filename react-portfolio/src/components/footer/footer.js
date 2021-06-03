c009c326417448d723ef0b4e681c3a06c1ba46db
Zhylow <pk1blue@yahoo.com>
2021-06-03 00:46:13 -0400

    fixed email in footer

--- react-portfolio/src/components/footer/footer.js
+++ react-portfolio/src/components/footer/footer.js
@@ -4,8 +4,8 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <p className="inLine">My email: <a id="myEmail" href="/">pk1blue@yahoo.com</a></p>
-            <p id="copyRight" className="inLine">My Phone# 845-545-4752</p>
+            <pre className="inLine">My email: pk1blue@yahoo.com</pre>
+            <pre id="copyRight" className="inLine">My Phone# 845-545-4752</pre>
             <a className=" inLine" href="https://drive.google.com/file/d/1Q4s-7X0M4wZqPPkuKNLE6PmPDnX_935T/view?usp=sharing" rel="noreferrer" target="_blank"><span className="navUniform">My Resume</span></a>
         </footer>
     )
