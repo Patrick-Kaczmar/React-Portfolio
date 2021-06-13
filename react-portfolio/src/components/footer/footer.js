81e9625f29fb2bc0aedf1a8138c44a8272eb32b8
Zhylow <pk1blue@yahoo.com>
2021-06-13 19:30:36 -0400

    updated resume link

--- react-portfolio/src/components/footer/footer.js
+++ react-portfolio/src/components/footer/footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <footer>
             <pre className="inLine">My email: pk1blue@yahoo.com</pre>
             <pre id="copyRight" className="inLine">My Phone# 845-545-4752</pre>
-            <a className=" inLine" href="https://drive.google.com/file/d/1Q4s-7X0M4wZqPPkuKNLE6PmPDnX_935T/view?usp=sharing" rel="noreferrer" target="_blank"><span className="navUniform">My Resume</span></a>
+            <a className=" inLine" href="https://drive.google.com/file/d/1Dw1jo3mcWaHiqUs9WMnc23nZDKzgSE0e/view?usp=sharing" rel="noreferrer" target="_blank"><span className="navUniform">My Resume</span></a>
         </footer>
     )
 }
